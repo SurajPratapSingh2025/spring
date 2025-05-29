907ca5b94e9c1f4675a4e7436fc7360d0302a5c7
SPS <123806275+SurajPratapSingh2025@users.noreply.github.com>
2025-05-30 02:54:23 +0530

    Add files via upload

---
+++ spring security/20:03:2025/springSecurity01/src/main/java/com/app/controller/MyController.java
@@ -0,0 +1,33 @@
+package com.app.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MyController {
+	
+	@GetMapping("/index")
+	public String indexPage() {
+		return "index";
+	}
+	
+	@GetMapping("/member")
+	public String memberPage() {
+		return "member";
+		
+	}
+	
+	@GetMapping("/admin")
+	public String adminPage() {
+		return "admin";
+	}
+	
+	@GetMapping("/error")
+	public String errorPage() {
+		return "error";
+	}
+
+	
+	
+	
+}
